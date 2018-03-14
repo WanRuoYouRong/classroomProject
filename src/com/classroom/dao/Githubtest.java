a35f96a3cd8ed963274ab7603b33816a5b2975b4
WanRuoYouRong <1020215351@qq.com>
2018-03-14 17:37:21 +0800

    Signed-off-by: WanRuoYouRong <1020215351@qq.com>  加了测试！

--- src/com/classroom/dao/Githubtest.java
+++ src/com/classroom/dao/Githubtest.java
@@ -5,6 +5,7 @@ public class Githubtest {
 	public static void main(String[] args) {
 		System.out.println("完全用于测试GitHub是否会实时更新！");
 		System.out.println("再次测试！");
+		System.out.println("再次试试！");
 	}
 
 }
